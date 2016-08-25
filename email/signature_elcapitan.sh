33f148e21a3ea141f8f9d77251af89593e6a3f4d
Brando Meniconi <meniconi.brando@gmail.com>
2016-08-25 15:09:18 +0200

    Create signature_elcapitan.sh

---
+++ email/signature_elcapitan.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+PLACEHOLDER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1);
+
+read -p "Create a new signature in Apple Mail, write inside only $PLACEHOLDER and save it, done?";
+killall Mail
+
+SIG_FILE=$(find ~/Library/Mail/V3/MailData/Signatures -type f -print0 | xargs -0 grep -l "$PLACEHOLDER")
+open -a TextEdit $SIG_FILE
+read -p "Replace the HTML code inside <body> tag in the TextEdit window and save it, done?";
+
+chflags uchg $SIG_FILE
+
+open -a Mail
+
+echo "Done.";
